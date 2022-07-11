b289145659d4f49c68f95b4a3902872ee5410cdf
Samuel Morales <sam@moralessa.dev>
2022-07-11 08:05:42 -0600

    finished hours and locations

--- modules/nav.js
+++ modules/nav.js
@@ -2,7 +2,10 @@ export default function exandMobileNav(){
     const menu = document.querySelector('.menu');
     if(menu.classList.contains('open')){
         menu.classList.remove('open');
+        setTimeout(() => {menu.style.display = 'none';}, 500)
+        
     }else{
-        menu.classList.add('open');
+        menu.style.display = 'block';
+        setTimeout(() => {menu.classList.add('open');}, 50)
     }
 }
