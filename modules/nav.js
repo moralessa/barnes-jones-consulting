ceefde1a7963c65dfc9ea3e2c458ec9dd9f9685f
Samuel Morales <sam@moralessa.dev>
2022-07-11 11:54:21 -0600

    finished services, biography, and get in touch sections

--- modules/nav.js
+++ modules/nav.js
@@ -2,8 +2,9 @@ export default function exandMobileNav(){
     const menu = document.querySelector('.menu');
     if(menu.classList.contains('open')){
         menu.classList.remove('open');
-        setTimeout(() => {menu.style.display = 'none';}, 500)
-        
+        setTimeout(() => {
+            menu.style.display = 'none';
+        }, 500)
     }else{
         menu.style.display = 'block';
         setTimeout(() => {menu.classList.add('open');}, 50)
