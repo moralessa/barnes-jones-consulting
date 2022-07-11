1c54513fccee1bfe32829538c2aea8577007cd6b
Samuel Morales <sam@moralessa.dev>
2022-07-11 07:01:01 -0600

    finished hero

--- modules/nav.js
+++ modules/nav.js
@@ -1,7 +1,6 @@
 export default function exandMobileNav(){
     const menu = document.querySelector('.menu');
     if(menu.classList.contains('open')){
-        console.log('called');
         menu.classList.remove('open');
     }else{
         menu.classList.add('open');
